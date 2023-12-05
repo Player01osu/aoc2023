1220030b93c9995a2078bf6a61af103cfb838aa3
player01osu <player01osu@gmail.com>
2023-12-05 17:13:52 -0500

    fast? day 1

--- day1.hs
+++ day1.hs
@@ -1,5 +1,6 @@
 import Data.Char (isDigit)
 import Data.List (isPrefixOf)
+import Debug.Trace (traceShowId)
 
 takeHeadLast (x:[]) = [x, x]
 takeHeadLast (x:xs) = [x, last xs]
@@ -7,16 +8,16 @@ takeHeadLast (x:xs) = [x, last xs]
 toString [] = []
 toString s@(c:cs)
   | isDigit c            = c:(toString cs)
-  | isPrefixOf "one" s   = '1':(toString cs)
-  | isPrefixOf "two" s   = '2':(toString cs)
-  | isPrefixOf "three" s = '3':(toString cs)
-  | isPrefixOf "four" s  = '4':(toString cs)
-  | isPrefixOf "five" s  = '5':(toString cs)
-  | isPrefixOf "six" s   = '6':(toString cs)
-  | isPrefixOf "seven" s = '7':(toString cs)
-  | isPrefixOf "eight" s = '8':(toString cs)
-  | isPrefixOf "nine" s  = '9':(toString cs)
-  | isPrefixOf "zero" s  = '0':(toString cs)
+  | isPrefixOf "one" s   = '1':(toString (drop 3 s))
+  | isPrefixOf "two" s   = '2':(toString (drop 3 s))
+  | isPrefixOf "three" s = '3':(toString (drop 5 s))
+  | isPrefixOf "four" s  = '4':(toString (drop 4 s))
+  | isPrefixOf "five" s  = '5':(toString (drop 4 s))
+  | isPrefixOf "six" s   = '6':(toString (drop 3 s))
+  | isPrefixOf "seven" s = '7':(toString (drop 5 s))
+  | isPrefixOf "eight" s = '8':(toString (drop 5 s))
+  | isPrefixOf "nine" s  = '9':(toString (drop 4 s))
+  | isPrefixOf "zero" s  = '0':(toString (drop 4 s))
   | otherwise            = toString cs
 
 takeDigits s = foldr (\x acc -> if isDigit x then x:acc else acc) [] s
