59ef311ee49bd8894d219dfc03babed11a797896
player01osu <player01osu@gmail.com>
2023-12-06 23:00:00 -0500

    Day one (maybe better?)

--- day1.hs
+++ day1.hs
@@ -1,24 +1,20 @@
-import Data.Char (isDigit)
+import Data.Char (isDigit, intToDigit)
 import Data.List (isPrefixOf)
-import Debug.Trace (traceShowId)
+import Data.Maybe (maybeToList)
 
 takeHeadLast (x:[]) = [x, x]
 takeHeadLast (x:xs) = [x, last xs]
 
 toString [] = []
 toString s@(c:cs)
-  | isDigit c            = c:(toString cs)
-  | isPrefixOf "one" s   = '1':(toString (drop 3 s))
-  | isPrefixOf "two" s   = '2':(toString (drop 3 s))
-  | isPrefixOf "three" s = '3':(toString (drop 5 s))
-  | isPrefixOf "four" s  = '4':(toString (drop 4 s))
-  | isPrefixOf "five" s  = '5':(toString (drop 4 s))
-  | isPrefixOf "six" s   = '6':(toString (drop 3 s))
-  | isPrefixOf "seven" s = '7':(toString (drop 5 s))
-  | isPrefixOf "eight" s = '8':(toString (drop 5 s))
-  | isPrefixOf "nine" s  = '9':(toString (drop 4 s))
-  | isPrefixOf "zero" s  = '0':(toString (drop 4 s))
-  | otherwise            = toString cs
+  | isDigit c = c:(toString cs)
+  | otherwise = (map intToDigit (maybeToList $ find digits s 0)) <> (toString cs)
+  where
+    digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    find [] _ _ = Nothing
+    find (s:xs) a acc
+      | isPrefixOf s a = Just acc
+      | otherwise      = find xs a (acc + 1)
 
 takeDigits s = foldr (\x acc -> if isDigit x then x:acc else acc) [] s
 
@@ -26,4 +22,4 @@ answerOne s = sum $ map (read . takeHeadLast . takeDigits) $ lines $ s
 
 answerTwo s = sum $ map (read . takeHeadLast . toString) $ lines $ s
 
-main = interact $ show . answerOne
+main = interact $ show . answerTwo
